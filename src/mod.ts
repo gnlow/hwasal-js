b3ebe9999dcf3f63718d372b3ec326c05a3b22d7
Gnlow <gnlow@protonmail.com>
2024-02-05 20:48:30 +0900

    use jsx

---
+++ src/mod.ts
@@ -0,0 +1,10 @@
+export declare namespace JSX {
+    interface IntrinsicElements {
+        [tag: string]: any
+    }
+}
+
+export const jsxTemplate = () => "hi"
+export const jsxAttr = () => "hi"
+export const jsxEscape = () => "hi"
+export const jsx = () => "hi"
